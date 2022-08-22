0254d8d60cc47e2356ae20eb2b607626b186afa7
JulianOrteil <dakota.horstman2001@gmail.com>
2022-08-21 23:03:31 -0500

    `Subsystem` definition

--- src/command_based_framework/subsystems.py
+++ src/command_based_framework/subsystems.py
@@ -1,2 +1,14 @@
 class Subsystem(object):
-    """Breaks out complex robot components into methods and attributes."""
+    """Breaks out complex robot components into methods and attributes.
+
+    Subsystems define how something is performed; i.e. reading a sensor.
+    Subsystems are also used by the scheduler to ensure two commands are
+    not using the same resources simultaneously.
+    """
+
+    def periodic(self) -> None:
+        """Periodically called when the subsystem is required by a scheduled :py:class:`command_based_framework.commands.Command`.
+
+        Override this behavior to always execute by calling
+        :py:meth:`~command_based_framework.scheduler.register_subsystem`.
+        """  # noqa: E501
