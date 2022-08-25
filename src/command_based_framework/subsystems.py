afa64d46ffcc171550439eb2af4ea303c5de24c2
JulianOrteil <dakota.horstman2001@gmail.com>
2022-08-25 11:55:40 -0500

    Add `current_command` and `default_command` properties to subsystems

--- src/command_based_framework/subsystems.py
+++ src/command_based_framework/subsystems.py
@@ -1,4 +1,10 @@
+from contextlib import suppress
+from typing import Optional
+
 from command_based_framework._common import ContextManagerMixin
+
+with suppress(ImportError):
+    from command_based_framework.commands import Command
 
 
 class Subsystem(ContextManagerMixin):
@@ -8,6 +14,47 @@ class Subsystem(ContextManagerMixin):
     Subsystems are also used by the scheduler to ensure two commands are
     not using the same resources simultaneously.
     """
+
+    # The command that is currently using this subsystem
+    _current_command: Optional["Command"]
+
+    # The command that will run whenever this subsystem is not being
+    # used
+    _default_command: Optional["Command"]
+
+    def __init__(self) -> None:
+        """Creates a new `Subsystem` instance."""
+        super().__init__()
+        self._current_command = None
+        self._default_command = None
+
+    @property
+    def current_command(self) -> Optional["Command"]:
+        """The command that is currently using this subsystem.
+
+        This property is controlled by the scheduler and should not be
+        modified directly.
+
+        If no default command is set and no command is currently using
+        this subsystem, then this property will be `None`.
+        """
+        return self._current_command
+
+    @current_command.setter
+    def current_command(self, command: Optional["Command"]) -> None:
+        self._current_command = command
+
+    @property
+    def default_command(self) -> Optional["Command"]:
+        """The command to run when no other command is active.
+
+        If not specified, then this subsystem will remain idle.
+        """
+        return self._default_command
+
+    @default_command.setter
+    def default_command(self, command: Optional["Command"]) -> None:
+        self._default_command = command
 
     def periodic(self) -> None:
         """Periodically called when the subsystem is required by a scheduled :py:class:`~command_based_framework.commands.Command`.
---
+++ tests/test_subsystems.py
@@ -0,0 +1,32 @@
+from command_based_framework.commands import Command
+from command_based_framework.subsystems import Subsystem
+
+
+def test_current_and_default_commands() -> None:
+    """Verify current and default commands get set"""
+    class MyCommand(Command):
+        def execute(self) -> None:
+            return super().execute()
+
+        def is_finished(self) -> bool:
+            return super().is_finished()
+
+    class MySubsystem(Subsystem):
+        def periodic(self) -> None:
+            return super().periodic()
+
+    command = MyCommand()
+    subsystem = MySubsystem()
+
+    # Verify the commands are set
+    subsystem.default_command = command
+    assert subsystem.default_command == command
+    assert subsystem.current_command == None
+
+    subsystem.current_command = command
+    assert subsystem.current_command == command
+
+    subsystem.current_command = None
+    subsystem.default_command = None
+    assert subsystem.default_command == None
+    assert subsystem.current_command == None
